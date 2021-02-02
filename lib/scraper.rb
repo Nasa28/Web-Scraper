05ee027454e297e096501101528f57ab07d74927
Nasa28 <kaluchinasaagu@gmail.com>
2021-02-02 19:05:30 +0100

    Fix rubocop errors

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,14 +3,14 @@ require 'httparty'
 require 'nokogiri'
 require 'csv'
 
-#Scraper class
+# Scraper class
 class Scraper
-
   attr_reader :url, :gadgets, :page
-  def initialize 
-  @url = 'https://www.jumia.com.ng/smartphones/'
-  @gadgets = Array.new
-  @page = 1
+
+  def initialize
+    @url = 'https://www.jumia.com.ng/smartphones/'
+    @gadgets = []
+    @page = 1
   end
 
   def url_data(url)
@@ -18,7 +18,7 @@ class Scraper
     Nokogiri::HTML(doc.body)
   end
 
-  def new_url_data(url)
+  def new_url_data(_url)
     new_url = "https://www.jumia.com.ng/smartphones/?page=#{@page}#catalog-listing"
     new_doc = HTTParty.get(new_url)
     Nokogiri::HTML(new_doc.body)
@@ -26,23 +26,21 @@ class Scraper
 
   def scraper
     file = url_data(@url)
-    items = file.css('div.info')
-    total_pages = 2400
-    last_page =50
-    CSV.open("file.csv", "wb") do |csv|
+    file.css('div.info')
+    CSV.open('file.csv', 'wb') do |csv|
       while @page <= 40
         new_file = new_url_data(@new_url)
         new_items = new_file.css('div.info')
         new_items.each do |item|
-            name = item.css('h3.name').text
+          name = item.css('h3.name').text
           price = item.css('div.prc').text
           discount_price = item.css('div.old').text
           rating = item.css('div.rev').text
           @gadgets.push(name.blue, price.green, discount_price.yellow, rating.red)
           csv << [name, price, discount_price, rating]
         end
-          @page += 1
-          puts @gadgets
+        @page += 1
+        puts @gadgets
 
       end
     end
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,10 +1,9 @@
 # spec/scraper_spec.rb
-require_relative '../lib/scraper.rb'
+require_relative '../lib/scraper'
 
 describe Scraper do
   let(:scrape) { Scraper.new }
   describe '#initialize' do
-
     it 'sets the url' do
       expect(scrape.url).to be_a(String)
     end
@@ -12,7 +11,7 @@ describe Scraper do
     it 'sets gadgets to an empty array' do
       expect(scrape.gadgets.empty?).to eq(true)
     end
-s
+    s
     it 'page should be a numeric and 1' do
       expect(scrape.page.is_a?(Numeric)).to eq(true)
     end
@@ -21,8 +20,7 @@ s
       expect(scrape.page.is_a?(String)).to eq(false)
     end
 
-    describe '#scraper' do 
-
+    describe '#scraper' do
       it 'returns an array of data' do
         expect(scrape.scraper.empty?).to eq(false)
       end
